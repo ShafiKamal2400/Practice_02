76b827a5af02929b23c8accae4cfaaf28410abe1
ShafiKamal2400 <go6803@wayne.edu>
2020-03-19 19:47:56 -0400

    A commit for extraCredit

---
+++ src/com/luv2code/servletdemo2/HelloWorld.java
@@ -0,0 +1,54 @@
+package com.luv2code.servletdemo2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// step1 content type
+		response.setContentType("text/html");
+		//printWriter
+		PrintWriter out = response.getWriter();
+		//generate HTML
+		out.println("<html><body>");
+				
+		out.println("<h2>Hello World</h2>");
+				
+		out.println("<hr>");
+		out.println("Time on server is: " + new java.util.Date());
+				
+		out.println("</html></body>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
